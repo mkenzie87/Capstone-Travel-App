24135cb00dac3727f8a4b4d3420b950225021dad
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-17 13:44:42 -0600

    Working with the first API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,58 @@
+//Get Input Fields
+
+const getLocation = document.getElementById('location').value;
+const tripDate = document.getElementById('trip-start').value;
+
+
+//API GEO Information
+const geoAPIBase = "http://api.geonames.org/searchJSON";
+const geoUser = "mkenzie87";
+const geoParams = `?q=${getLocation}&maxRows=10&username=${geoUser}`;
+const geoFetch = geoAPIBase + geoParams;
+
+
 function handleSubmit(event) {
-    const getLocation = document.getElementById('location').value;
-    const tripDate = document.getElementById('trip-start').value;
-    console.log(getLocation);
 
-    //API GEO Information
-    const geoAPIBase = "http://api.geonames.org/searchJSON";
-    const geoUser = "mkenzie87";
-    const geoParams = `?q=${getLocation}&maxRows=10&username=${geoUser}`;
-    const geoFetch = geoAPIBase + geoParams;
+    console.log(geoFetch); // logging fetch Url
 
-    console.log(geoFetch);
+    getLocationData(geoFetch) // this will trigger the getLocationData function
+      // New Syntax!
+      .then(function(data) {
+        // Add data
+        console.log(data);
+        // postData('/addWeather', {
+        //   date: newDate,
+        //   name: data.name,
+        //   temp: data.main.temp,
+        //   feels_like: data.main.feels_like,
+        //   temp_max: data.main.temp_max,
+        //   temp_desc: data.weather[0].description,
+        //   content: newContent
+        // })
+      })
+      .then(function(newData) {
+
+        updateUI()
+      })
 
 }
 
-    export {
+const getLocationData = async (geoFetch) => {
+
+  const res = await fetch(geoFetch)
+  try {
+
+    const data = await res.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+
+
+export {
       handleSubmit
     }
