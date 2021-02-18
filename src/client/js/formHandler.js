b387f470ef2eb9f69816792fbcf561ab104e8489
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-17 23:36:50 -0600

    GEO APi working with lng and lat now working on date data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,8 +1,6 @@
 //Get Input Fields
-
 const getLocation = document.getElementById('location').value;
 const tripDate = document.getElementById('trip-start').value;
-
 
 //API GEO Information
 const geoAPIBase = "http://api.geonames.org/searchJSON";
@@ -11,24 +9,32 @@ const geoParams = `?q=${getLocation}&maxRows=10&username=${geoUser}`;
 const geoFetch = geoAPIBase + geoParams;
 
 
+//API Weatherbit Information
+//Example Link:
+//https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely
+const weatherAPIBase = "https://api.weatherbit.io/v2.0/current";
+const weatherAPIKey = "e9828cd2c56e45fcaefec003e2efc322"
+
+
+
 function handleSubmit(event) {
+    event.preventDefault()
 
     console.log(geoFetch); // logging fetch Url
+    console.log(tripDate); // logging date test
 
     getLocationData(geoFetch) // this will trigger the getLocationData function
       // New Syntax!
       .then(function(data) {
+          const geoLat = data.geonames[0].lat; // storing location latitude
+          const geoLng = data.geonames[0].lng; // storing location longitute
+
+          console.log(geoLat + " Lat Data") // logging out location latitue
+          console.log(geoLng + " Lng Data") // logging out location longitute
+
         // Add data
         console.log(data);
-        // postData('/addWeather', {
-        //   date: newDate,
-        //   name: data.name,
-        //   temp: data.main.temp,
-        //   feels_like: data.main.feels_like,
-        //   temp_max: data.main.temp_max,
-        //   temp_desc: data.weather[0].description,
-        //   content: newContent
-        // })
+
       })
       .then(function(newData) {
 
@@ -43,14 +49,40 @@ const getLocationData = async (geoFetch) => {
   try {
 
     const data = await res.json();
+    console.log("made it in function");
     console.log(data)
     return data;
   } catch (error) {
     console.log("error", error);
+    console.log("made it in error function");
     // appropriately handle the error
   }
 }
 
+
+
+
+function dateCompare (e) {
+
+  // Current Data Variables
+  const date = new Date(); // gets Current Date
+  const day = date.getDate(); // gets current day
+  const month = date.getMonth() + 1; // gets current month need to add 1 because it starts with 0
+  const year = date.getFullYear(); // get current month
+  const currentFullDate = parseInt(`${year},${month},${day}`); // Full Current Date
+  const currentUTC = Date.UTC(currentFullDate);
+
+  //Selected Trip Date
+  const selectedDate = parseInt(tripDate); // Selected Trip Date
+  const selectedUTC = Date.UTC(selectedDate);
+
+  // Comparing Days
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const compareDate =  Math.abs((selectedUTC - currentUTC) / _MS_PER_DAY);
+
+  console.log(compareDate)
+
+}
 
 
 export {
--- src/server/index.js
+++ src/server/index.js
@@ -32,127 +32,127 @@ app.get('/', function(req, res) {
 //   res.send(mockAPIResponse)
 // })
 
-
-app.post('/process-text', function(req, res) {
-
-  // meaningcloud api structure
-  // let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=APIKEYHERE&lang=en&txt=hello&model=general';
-
-  // Establishing API Info
-  const apiKey = process.env.API_KEY; // meaningcloud API key hidden
-  const articleURL = req.body.articleURL;
-
-
-
-  // Building Fetch URL Using Base Url then adding Params with API Key and URL Info
-  const baseURL = 'https://api.meaningcloud.com/sentiment-2.1' //meaningcloud base request url
-  const allParams = `?key=${apiKey}&lang=en&model=general&url=${articleURL}` // adding API Params
-  const fetchURL = baseURL + allParams; // builing full Fetch Url
-
-  console.log(fetchURL); // logging Fetch URL
-
-  fetch(fetchURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  }).then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-    res.send({
-      irony: data.irony,
-      agreement: data.agreement,
-      subjectivity: data.subjectivity,
-      confidence: data.confidence,
-      status: data.status.msg,
-    })
-  });
-})
-
-
-
-
-
-
-
-
-app.post('/process-open', function(req, res) {
-
-  // meaningcloud api structure
-  // let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=APIKEYHERE&lang=en&txt=hello&model=general';
-
-  // Establishing API Info
-  const apiKey = process.env.API_KEY; // meaningcloud API key hidden
-  const articleURL = req.body.articleURL;
-
-
-
-  // Building Fetch URL Using Base Url then adding Params with API Key and URL Info
-  const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=' //meaningcloud base request url
-  const allParams = `,us&units=imperial&appid=${apiKey}&url=${articleURL}` // adding API Params
-  const fetchURL = baseURL + allParams; // builing full Fetch Url
-
-  console.log(fetchURL); // logging Fetch URL
-
-  fetch(fetchURL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  }).then((response) => {
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-    res.send({
-      date: data.date,
-      temp_desc:  data.temp_desc,
-      name: data.name,
-      temp: data.temp,
-      temp_max: data.temp_max,
-      content: data.content,
-    })
-  });
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// GET Weather Data
-app.get('/allWeatherData', sendWeatherData);
-
-function sendWeatherData(req, res) {
-  res.send(projectData);
-};
-
-
-// POST Weather Data
-const data = [];
-app.post('/addWeather', addWeather);
-
-function addWeather(req, res) {
-  projectData['date'] = req.body.date;
-  projectData['temp_desc'] = req.body.temp_desc;
-  projectData['name'] = req.body.name;
-  projectData['temp'] = req.body.temp;
-  projectData['feels_like'] = req.body.feels_like;
-  projectData['temp_max'] = req.body.temp_max;
-  projectData['content'] = req.body.content;
-  res.send(projectData);
-}
+//
+// app.post('/process-text', function(req, res) {
+//
+//   // meaningcloud api structure
+//   // let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=APIKEYHERE&lang=en&txt=hello&model=general';
+//
+//   // Establishing API Info
+//   const apiKey = process.env.API_KEY; // meaningcloud API key hidden
+//   const articleURL = req.body.articleURL;
+//
+//
+//
+//   // Building Fetch URL Using Base Url then adding Params with API Key and URL Info
+//   const baseURL = 'https://api.meaningcloud.com/sentiment-2.1' //meaningcloud base request url
+//   const allParams = `?key=${apiKey}&lang=en&model=general&url=${articleURL}` // adding API Params
+//   const fetchURL = baseURL + allParams; // builing full Fetch Url
+//
+//   console.log(fetchURL); // logging Fetch URL
+//
+//   fetch(fetchURL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//   }).then((response) => {
+//     return response.json();
+//   }).then((data) => {
+//     console.log(data);
+//     res.send({
+//       irony: data.irony,
+//       agreement: data.agreement,
+//       subjectivity: data.subjectivity,
+//       confidence: data.confidence,
+//       status: data.status.msg,
+//     })
+//   });
+// })
+//
+//
+//
+//
+//
+//
+//
+//
+// app.post('/process-open', function(req, res) {
+//
+//   // meaningcloud api structure
+//   // let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=APIKEYHERE&lang=en&txt=hello&model=general';
+//
+//   // Establishing API Info
+//   const apiKey = process.env.API_KEY; // meaningcloud API key hidden
+//   const articleURL = req.body.articleURL;
+//
+//
+//
+//   // Building Fetch URL Using Base Url then adding Params with API Key and URL Info
+//   const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=' //meaningcloud base request url
+//   const allParams = `,us&units=imperial&appid=${apiKey}&url=${articleURL}` // adding API Params
+//   const fetchURL = baseURL + allParams; // builing full Fetch Url
+//
+//   console.log(fetchURL); // logging Fetch URL
+//
+//   fetch(fetchURL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//   }).then((response) => {
+//     return response.json();
+//   }).then((data) => {
+//     console.log(data);
+//     res.send({
+//       date: data.date,
+//       temp_desc:  data.temp_desc,
+//       name: data.name,
+//       temp: data.temp,
+//       temp_max: data.temp_max,
+//       content: data.content,
+//     })
+//   });
+// })
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // GET Weather Data
+// app.get('/allWeatherData', sendWeatherData);
+//
+// function sendWeatherData(req, res) {
+//   res.send(projectData);
+// };
+//
+//
+// // POST Weather Data
+// const data = [];
+// app.post('/addWeather', addWeather);
+//
+// function addWeather(req, res) {
+//   projectData['date'] = req.body.date;
+//   projectData['temp_desc'] = req.body.temp_desc;
+//   projectData['name'] = req.body.name;
+//   projectData['temp'] = req.body.temp;
+//   projectData['feels_like'] = req.body.feels_like;
+//   projectData['temp_max'] = req.body.temp_max;
+//   projectData['content'] = req.body.content;
+//   res.send(projectData);
+// }
