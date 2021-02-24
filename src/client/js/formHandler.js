7a8cef45342debc3d818ab69f9245da02611c750
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-23 23:28:50 -0600

    Building updateUI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,7 +9,7 @@ const geoFetch = geoAPIBase + geoParams;
 
 //API Weatherbit Information
 const weatherCurrentAPIBase = "https://api.weatherbit.io/v2.0/forecast/daily";
-const weatherHistoryAPIBase = "http://api.weatherbit.io/v2.0/history/daily";
+const weatherHistoryAPIBase = "https://api.weatherbit.io/v2.0/history/daily";
 const weatherAPIKey = "e9828cd2c56e45fcaefec003e2efc322"
 
 //PixaBay API Information:
@@ -21,6 +21,7 @@ const pixaBayAPIBase = "https://pixabay.com/api/?key="
 function handleSubmit(event) {
     event.preventDefault()
     const getLocation = document.getElementById('location').value;
+    const tripDate = document.getElementById('trip-start').value; // Set Date for Trip
 
 
     getLocationData(geoAPIBase, getLocation) // this will trigger the getLocationData function
@@ -36,18 +37,19 @@ function handleSubmit(event) {
         console.log(geoData);
 
         // Run dateCompare Function to see Days between
-        const daysBetween = dateCompare();
-        console.log(daysBetween); // log days between number
+        const daysBetween = dateCompare(tripDate);
+        console.log("Days between", daysBetween); // log days between number
 
         let weatherAPI = ""; // set weatherAPI to empty string to hold Weather API
-        if (daysBetween > 7) {
-          //https:api.weatherbit.io/v2.0/history/daily?lat=38.123&lon=-78.543&start_date=2021-02-14&end_date=2021-02-15
-          console.log("Made it in the History API" + daysBetween);
-          weatherAPI = `${weatherHistoryAPIBase}?lat=${geoLat}&lon=${geoLng}&start_date=${tripDate}&end_date=2021-02-30`;
-        } else {
+        if (daysBetween <= 7) {
           //https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely
           console.log("Made it in the Current API", daysBetween);
           weatherAPI = `${weatherCurrentAPIBase}?lat=${geoLat}&lon=${geoLng}&key=${weatherAPIKey}`;
+          } else {
+          //https:api.weatherbit.io/v2.0/history/daily?lat=38.123&lon=-78.543&start_date=2021-02-14&end_date=2021-02-15
+          console.log("Made it in the History API" + daysBetween);
+          weatherAPI = `${weatherHistoryAPIBase}?lat=${geoLat}&lon=${geoLng}&start_date=${tripDate}&end_date=2021-02-23&key=${weatherAPIKey}`;
+
         }
 
         return getWeatherData(weatherAPI); // run getWeatherData to return fetched data
@@ -61,9 +63,9 @@ function handleSubmit(event) {
         const stateName = weatherData.state_code; // storying state abbrev
 
 
-        console.log("High Temp ", highTemp);
-        console.log("Low Temp ", lowTemp);
-        console.log("State Code ", stateName);
+        // console.log("High Temp ", highTemp);
+        // console.log("Low Temp ", lowTemp);
+        // console.log("State Code ", stateName);
 
         return getImageData(getLocation);
       })
@@ -75,17 +77,19 @@ function handleSubmit(event) {
 
         console.log("Web Image URL: ", webImage);
         console.log("Large Image URL: ", largeImage);
-
-      });
-
-
-
-
-
-      // .then(function(newData) {
       //
-      //   updateUI()
+      //   postData('/addWeather', {
+      //     web_image: imageData.hits[0].webformatURL,
+      //     large_image: imageData.hits[0].largeImageURL
       // })
+
+      })
+
+
+      .then(function(newData) {
+
+        updateUI(newData)
+      })
 
 }
 
@@ -135,9 +139,9 @@ const getImageData = async (city) => {
 
 
 
-const dateCompare = async (geoLat, geoLng) => {
+function dateCompare(tripDate) {
 
-  const tripDate = document.getElementById('trip-start').value; // Set Date for Trip
+
   const setTripDate = new Date(tripDate); //Adding Set Date Normal Formatting
 
 // I am having an issue with this if i wrap it in a setDate() comes back with a long string of numbers
@@ -157,7 +161,7 @@ const dateCompare = async (geoLat, geoLng) => {
   const _MS_PER_DAY = 1000 * 60 * 60 * 24;
   const compareDate =  Math.floor((currentUTC - setTripUTC) / _MS_PER_DAY);
 
-
+  console.log(compareDate);
   return compareDate;
 
 }
@@ -167,19 +171,32 @@ const updateUI = async () => {
   const request = await fetch('/allWeatherData');
   try {
     const allData = await request.json();
-    document.getElementById('date').innerHTML = allData.date;
-    document.getElementById('location').innerHTML = allData.name;
-    document.getElementById('temp_desc').innerHTML = allData.temp_desc;
-    document.getElementById('temp').innerHTML = allData.temp;
-    document.getElementById('feels-like').innerHTML = allData.feels_like;
-    document.getElementById('max-temp').innerHTML = allData.temp_max;
-    document.getElementById('content').innerHTML = allData.content;
+    document.getElementById('city_field').innerHTML = allData.web_image;
+    document.getElementById('city_image').innerHTML = allData.large_image;
 
   } catch (error) {
     console.log("error", error);
   }
 }
 
+const postData = async (url = '', data = {}) => {
+
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 export {
       handleSubmit
