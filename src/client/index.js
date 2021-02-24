535ee1555460f22ea6364c8f0a6ae823288a5691
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-24 01:11:01 -0600

    Adding Styles

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import { handleSubmit } from './js/formHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
+import './styles/location.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
